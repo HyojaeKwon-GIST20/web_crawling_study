1a8e0db9854d716d626779205e778b4ff10476b5
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2020-09-11 17:04:00 +0900

    third

--- venv/web_crawling_test.py
+++ venv/web_crawling_test.py
@@ -1,17 +1,23 @@
 from urllib.request import urlopen
 from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
+import urllib.request
 
-base_url = 'https://search.naver.com/search.naver?where=post&sm=tab_jum&query='
+base_url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
 plus_url = input('검색하고 싶은 단어를 넣어 주셈')
 
 url = base_url+quote_plus(plus_url)
 
-html =urlopen(url).read()
-
-
-
+html=urlopen(url).read()
 soup=BeautifulSoup(html,'html.parser')
-title =soup.find_all(class ='sh_blog_title')
+
+print(url)
+image=soup.find_all(class_='_img')
 
 
+n=1
+for i in image:
+    img_url=i['data-source']
+    print(img_url)
+    urllib.request.urlretrieve(img_url,'C:/Users/khj01/Desktop/hi/'+str(n)+'.jpg')
+    n+=1
